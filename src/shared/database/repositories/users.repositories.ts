0db63f78020749c9cb2d9e5e272802180b44b053
moraiskaua <moraiskaua03@gmail.com>
2024-02-20 13:52:43 -0300

    feat: create categories module

--- src/shared/database/repositories/users.repositories.ts
+++ src/shared/database/repositories/users.repositories.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from '../prisma.service';
 import { type Prisma } from '@prisma/client';
 
 @Injectable()
